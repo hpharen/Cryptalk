fb142ff8fd83ba8d36f827b44f4bb55cb7f00d38
Haren Patel <hp.haren@gmail.com>
2025-01-15 16:27:18 -0500

    added to database on psql and server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post('/login', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5090;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
